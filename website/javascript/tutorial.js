74cc08cd325ce27f47617e52e9f2684e31134570
Crystal <crystal.wang@student.uq.edu.au>
2026-08-22 14:34:26 +1000

    added js and tutorial page

---
+++ website/javascript/tutorial.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed('#typed-text', {
+    strings: ['beautiful websites.', 'fast applications.', 'cool animations.'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+  });
+});
